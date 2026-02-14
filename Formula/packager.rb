a7b66e88c2d3b615af220156d004580760dd40f7
moohaad <40121+moohaad@users.noreply.github.com>
2026-02-14 16:23:48 +0300

    Update Packager to version 3.4.2 with new URL and SHA256

--- Formula/packager.rb
+++ Formula/packager.rb
@@ -1,9 +1,9 @@
 class Packager < Formula
   desc "Tool and a media packaging SDK for DASH and HLS packaging and encryption"
   homepage "https://shaka-project.github.io/shaka-packager/"
-  url "https://github.com/shaka-project/shaka-packager/releases/download/v3.0.0/packager-osx-x64"
-  version "3.0.0"
-  sha256 "5bd4cc4cca6f46fd6be88a56e82a100a2c2c383a7ed53fd5a49d3ad760a4fbb3"
+  url "https://github.com/shaka-project/shaka-packager/releases/download/v3.4.2/packager-osx-arm64"
+  version "3.4.2"
+  sha256 "d97cc27e2b003dd56937c54eb08f0c3f61637ad976a2d98b8bf2adaf041cb1c2"
   license "BSD-3-Clause"
 
   def install
